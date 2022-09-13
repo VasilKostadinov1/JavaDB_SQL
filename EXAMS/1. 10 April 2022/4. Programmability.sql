1e193f6d81a2bf9bf551a958c11cebf1a6f16ee1
VasilKostadinov1 <90925326+VasilKostadinov1@users.noreply.github.com>
2022-09-13 11:29:45 +0300

    Add files via upload

---
+++ EXAMS/1. 10 April 2022/4. Programmability.sql
@@ -0,0 +1,39 @@
+# 10 10.	History movies----------------------------------
+
+DELIMITER $$
+CREATE FUNCTION udf_actor_history_movies_count(full_name VARCHAR(50))
+    RETURNS INT
+    DETERMINISTIC
+BEGIN
+    DECLARE movies_count INT;
+    SET movies_count := (
+        SELECT COUNT(g.name) movies
+        FROM actors a
+                 JOIN movies_actors ma on a.id = ma.actor_id
+                 JOIN genres_movies gm on ma.movie_id = gm.movie_id
+                 JOIN genres g on g.id = gm.genre_id
+        WHERE CONCAT(a.first_name, ' ', a.last_name) = full_name AND g.name = 'History'
+        GROUP BY  g.name);
+    RETURN movies_count;
+end $$
+DELIMITER ;
+
+SELECT udf_actor_history_movies_count('Stephan Lundberg')  AS 'history_movies';
+SELECT udf_actor_history_movies_count('Jared Di Batista')  AS 'history_movies';
+SELECT udf_actor_history_movies_count('Lucretia Binks')  AS 'history_movies';
+
+# 11 11.	Movie awards -------------------------------------------------------------
+
+DELIMITER $$
+CREATE PROCEDURE `udp_award_movie`(`movie_title` VARCHAR(50))
+BEGIN
+    UPDATE actors a
+        JOIN movies_actors ma on a.id = ma.actor_id
+        JOIN movies m on m.id = ma.movie_id
+    SET  a.awards = a.awards + 1
+    WHERE m.title = movie_title;
+END $$
+DELIMITER ;
+
+CALL udp_award_movie('Tea For Two');
+CALL udp_award_movie('Miss You Can Do It');
